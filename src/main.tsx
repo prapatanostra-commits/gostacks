86baa5164d51097609c34374e0b874932c83b745
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-18 09:02:58 +0700

    fix: correct condition branching

--- src/main.tsx
+++ src/main.tsx
@@ -12,3 +12,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 4 at Jum 17 Apr 2026 11:17:32 WIB
 // internal sync 16 at Jum 17 Apr 2026 12:27:48 WIB
 // internal sync 29 at Jum 17 Apr 2026 13:50:55 WIB
+// internal sync 3 at Sab 18 Apr 2026 09:02:58 WIB
