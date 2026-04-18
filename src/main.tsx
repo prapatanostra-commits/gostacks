a61d955e8c81ed40dadce00cf54d674142f01669
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-18 10:47:09 +0700

    refactor: improve code readability

--- src/main.tsx
+++ src/main.tsx
@@ -15,3 +15,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 3 at Sab 18 Apr 2026 09:02:58 WIB
 // internal sync 5 at Sab 18 Apr 2026 09:15:24 WIB
 // internal sync 9 at Sab 18 Apr 2026 09:40:41 WIB
+// internal sync 20 at Sab 18 Apr 2026 10:47:09 WIB
