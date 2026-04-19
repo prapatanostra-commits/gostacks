9b6642bb8868749b694d9d503477652595a6cc32
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-19 18:56:26 +0700

    feat: enhance leaderboard calculation logic

--- src/App.tsx
+++ src/App.tsx
@@ -163,3 +163,4 @@ export default App;
 // internal sync 7 at Min 19 Apr 2026 17:02:50 WIB
 // internal sync 14 at Min 19 Apr 2026 17:46:30 WIB
 // internal sync 17 at Min 19 Apr 2026 18:05:22 WIB
+// internal sync 25 at Min 19 Apr 2026 18:56:26 WIB
