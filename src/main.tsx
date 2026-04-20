f45c90881c43cd6d32759b83c065fa51fb3a9f95
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 03:35:02 +0700

    fix: handle edge case scenario

--- src/main.tsx
+++ src/main.tsx
@@ -66,3 +66,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 132 at Sel 21 Apr 2026 00:14:58 WIB
 // internal sync 133 at Sel 21 Apr 2026 00:18:46 WIB
 // internal sync 160 at Sel 21 Apr 2026 02:20:24 WIB
+// internal sync 178 at Sel 21 Apr 2026 03:35:02 WIB
