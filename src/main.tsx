8244add922d6d094d6cc5e782779ede5e3172d38
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 18:33:10 +0700

    docs: clarify notes for developers

--- src/main.tsx
+++ src/main.tsx
@@ -83,3 +83,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 40 at Sel 21 Apr 2026 16:33:54 WIB
 // internal sync 55 at Sel 21 Apr 2026 17:41:26 WIB
 // internal sync 58 at Sel 21 Apr 2026 17:56:26 WIB
+// internal sync 66 at Sel 21 Apr 2026 18:33:10 WIB
