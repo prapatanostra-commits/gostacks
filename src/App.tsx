e5ef463c0f8ff0178a67aa0f152357958d713f82
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-22 06:24:54 +0700

    feat: introduce utility helpers

--- src/App.tsx
+++ src/App.tsx
@@ -233,3 +233,4 @@ export default App;
 // internal sync 203 at Rab 22 Apr 2026 04:51:27 WIB
 // internal sync 204 at Rab 22 Apr 2026 04:55:42 WIB
 // internal sync 217 at Rab 22 Apr 2026 05:56:05 WIB
+// internal sync 223 at Rab 22 Apr 2026 06:24:54 WIB
