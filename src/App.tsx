89ec43609e942c72432366df635cac7136679ee1
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 18:44:57 +0700

    docs: clarify notes for developers

--- src/App.tsx
+++ src/App.tsx
@@ -218,3 +218,4 @@ export default App;
 // internal sync 54 at Sel 21 Apr 2026 17:35:32 WIB
 // internal sync 59 at Sel 21 Apr 2026 18:01:34 WIB
 // internal sync 63 at Sel 21 Apr 2026 18:18:48 WIB
+// internal sync 69 at Sel 21 Apr 2026 18:44:57 WIB
