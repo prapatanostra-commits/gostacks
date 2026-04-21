c87ae3918665d7c90d6231dada5b40c80a5b613a
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 21:05:03 +0700

    fix: adjust UI rendering issue

--- src/App.tsx
+++ src/App.tsx
@@ -221,3 +221,4 @@ export default App;
 // internal sync 69 at Sel 21 Apr 2026 18:44:57 WIB
 // internal sync 82 at Sel 21 Apr 2026 19:44:06 WIB
 // internal sync 93 at Sel 21 Apr 2026 20:35:14 WIB
+// internal sync 99 at Sel 21 Apr 2026 21:05:03 WIB
