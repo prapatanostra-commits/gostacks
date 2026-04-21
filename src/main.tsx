bfe60dccf739e953be2317581e23d56192c4cf75
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-21 17:41:26 +0700

    docs: expand security documentation

--- src/main.tsx
+++ src/main.tsx
@@ -81,3 +81,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 286 at Sel 21 Apr 2026 11:56:02 WIB
 // internal sync 37 at Sel 21 Apr 2026 16:17:52 WIB
 // internal sync 40 at Sel 21 Apr 2026 16:33:54 WIB
+// internal sync 55 at Sel 21 Apr 2026 17:41:26 WIB
