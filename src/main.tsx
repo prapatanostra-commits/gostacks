fad3e837a24aa88f310795848d223ebe34188741
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-22 17:20:11 +0700

    refactor: reorganize component hierarchy

--- src/main.tsx
+++ src/main.tsx
@@ -102,3 +102,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 13 at Rab 22 Apr 2026 15:23:58 WIB
 // internal sync 14 at Rab 22 Apr 2026 15:27:37 WIB
 // internal sync 25 at Rab 22 Apr 2026 16:16:25 WIB
+// internal sync 38 at Rab 22 Apr 2026 17:20:11 WIB
