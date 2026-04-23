bb20be49589bfc44172b34c28ff680883d13eec2
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 19:24:09 +0700

    fix: adjust UI rendering issue

--- src/main.tsx
+++ src/main.tsx
@@ -127,3 +127,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 55 at Kam 23 Apr 2026 16:45:00 WIB
 // internal sync 62 at Kam 23 Apr 2026 17:12:18 WIB
 // internal sync 84 at Kam 23 Apr 2026 18:43:07 WIB
+// internal sync 94 at Kam 23 Apr 2026 19:24:09 WIB
