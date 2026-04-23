e7bf31ca32f5725069d4a448ced40c96b90023a3
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-24 01:44:54 +0700

    refactor: simplify contract structure

--- src/App.tsx
+++ src/App.tsx
@@ -271,3 +271,4 @@ export default App;
 // internal sync 122 at Kam 23 Apr 2026 21:23:59 WIB
 // internal sync 143 at Kam 23 Apr 2026 22:49:52 WIB
 // internal sync 157 at Kam 23 Apr 2026 23:45:26 WIB
+// internal sync 187 at Jum 24 Apr 2026 01:44:54 WIB
