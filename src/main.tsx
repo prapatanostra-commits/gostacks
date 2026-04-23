8fa8ce783d46484fef49fce4974bedf10b82810b
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 17:12:18 +0700

    docs: enhance explanation details

--- src/main.tsx
+++ src/main.tsx
@@ -125,3 +125,4 @@ createRoot(document.getElementById('root')!).render(
 // internal sync 63 at Kam 23 Apr 2026 12:55:24 WIB
 // internal sync 22 at Kam 23 Apr 2026 14:30:41 WIB
 // internal sync 55 at Kam 23 Apr 2026 16:45:00 WIB
+// internal sync 62 at Kam 23 Apr 2026 17:12:18 WIB
