5791d98d8931577c2b8efefdef6c5955ca282b3f
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-23 15:35:01 +0700

    chore: clean code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -263,3 +263,4 @@ export default App;
 // internal sync 27 at Kam 23 Apr 2026 10:29:07 WIB
 // internal sync 56 at Kam 23 Apr 2026 12:24:44 WIB
 // internal sync 27 at Kam 23 Apr 2026 14:51:21 WIB
+// internal sync 38 at Kam 23 Apr 2026 15:35:01 WIB
