359ef0a6c85e5299cf714e7da28cec9462f496b6
prapatanostra-commits <prapatanostra@gmail.com>
2026-04-24 08:19:56 +0700

    feat: introduce helper abstraction layer

--- src/App.tsx
+++ src/App.tsx
@@ -275,3 +275,4 @@ export default App;
 // internal sync 207 at Jum 24 Apr 2026 03:08:06 WIB
 // internal sync 210 at Jum 24 Apr 2026 03:19:37 WIB
 // internal sync 248 at Jum 24 Apr 2026 05:51:11 WIB
+// internal sync 1 at Jum 24 Apr 2026 08:19:56 WIB
